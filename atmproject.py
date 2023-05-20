63267b915896d83d58c7fa1fde829afec6da3d0c
kennataddese6 <kennataddese6@gmail.com>
2023-05-20 15:41:22 +0300

    sceduled sending of data using python timer

---
+++ atmproject.py
@@ -0,0 +1,8 @@
+import time
+import shutil
+
+while 1 == 1:
+        wait = time.sleep(1)
+        shutil.copy("C:\\Users\\CBE\\Documents\\atm\\atmdata.txt",
+                "C:\\Users\\CBE\\Documents\\server\\atmdata.txt")
+
